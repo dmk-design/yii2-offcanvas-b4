9970fbe8892ddcae81e9efe9bc8f0be85de1ea1d
Aaron Kennedy <aaron@dmkdesign.ca>
2023-11-14 10:30:15 -0800

    Update offcanvas.js

--- src/assets/offcanvas.js
+++ src/assets/offcanvas.js
@@ -5,78 +5,78 @@
  * @author Aaron Kennedy aaron@dmkdesign.ca
  * Thanks to Amit at https://fellowtuts.com/html-css/off-canvas-menu-sidebar-overlay/ 
  */
-$(document).ready(function () {
+
 	var bsOverlay;
 
-	function OffCanvas(canvasId, options={})
-	{
-		this.defaults = {
-			insertBackdrop: true,
-		};
+	// function OffCanvas(canvasId, options={})
+	// {
+	// 	this.defaults = {
+	// 		insertBackdrop: true,
+	// 	};
 
-		this.selector = canvasId;
-		this.canvas = $("#"+canvasId);
+	// 	this.selector = canvasId;
+	// 	this.canvas = $("#"+canvasId);
 
-		this.dialog = $(canvasId).find('.offcanvas-dialog');
+	// 	this.dialog = $(canvasId).find('.offcanvas-dialog');
 
-		this.header = $(canvasId).find('.offcanvas-header');
+	// 	this.header = $(canvasId).find('.offcanvas-header');
 
-		this.content = $(canvasId).find('.offcanvas-body');
+	// 	this.content = $(canvasId).find('.offcanvas-body');
 
-		this.footer = $(canvasId).find('.offcanvas-footer');
+	// 	this.footer = $(canvasId).find('.offcanvas-footer');
 
-		this.insertBackdrop = function()
-		{
-			$('body').prepend('<div class=\"bs-canvas-overlay bg-dark position-fixed w-100 h-100\"></div>');
-			bsOverlay = $('.bs-canvas-overlay');
-		}
+	// 	this.insertBackdrop = function()
+	// 	{
+	// 		$('body').prepend('<div class=\"bs-canvas-overlay bg-dark position-fixed w-100 h-100\"></div>');
+	// 		bsOverlay = $('.bs-canvas-overlay');
+	// 	}
 
-		this.show = function()
-		{
-			bsOverlay = $('.bs-canvas-overlay');
-			this.canvas.addClass('mr-0');
-			$(this.selector + ' .bs-canvas-close').attr('aria-expanded', "true");
-			$('[data-target="' + this.selector + '"], a[href="' + this.selector + '"]').attr('aria-expanded', "true");
-			if(bsOverlay.length)
-				bsOverlay.addClass('show');
-			return false;
-		}
+	// 	this.show = function()
+	// 	{
+	// 		bsOverlay = $('.bs-canvas-overlay');
+	// 		this.canvas.addClass('mr-0');
+	// 		$(this.selector + ' .bs-canvas-close').attr('aria-expanded', "true");
+	// 		$('[data-target="' + this.selector + '"], a[href="' + this.selector + '"]').attr('aria-expanded', "true");
+	// 		if(bsOverlay.length)
+	// 			bsOverlay.addClass('show');
+	// 		return false;
+	// 	}
 
-		this.hide = function()
-		{
-			elm.removeClass('mr-0');
-			$('.bs-canvas-close', elm).attr('aria-expanded', "false");
-			if(bsOverlay.length)
-				bsOverlay.removeClass('show');
-		}
+	// 	this.hide = function()
+	// 	{
+	// 		elm.removeClass('mr-0');
+	// 		$('.bs-canvas-close', elm).attr('aria-expanded', "false");
+	// 		if(bsOverlay.length)
+	// 			bsOverlay.removeClass('show');
+	// 	}
 
-		 /**
-     * Set modal content
-     * @param {string} content The content of modal content
-     */
-		 this.setContent = function (content) {
-			$(this.content).html(content);
-		};
+	// 	 /**
+    //  * Set modal content
+    //  * @param {string} content The content of modal content
+    //  */
+	// 	 this.setContent = function (content) {
+	// 		$(this.content).html(content);
+	// 	};
 	
-		/**
-		 * Set modal footer
-		 * @param {string} content The content of modal footer
-		 */
-		this.setFooter = function (content) {
-			$(this.footer).html(content);
-		};
+	// 	/**
+	// 	 * Set modal footer
+	// 	 * @param {string} content The content of modal footer
+	// 	 */
+	// 	this.setFooter = function (content) {
+	// 		$(this.footer).html(content);
+	// 	};
 	
-		/**
-		 * Set modal title
-		 * @param {string} title The title of modal
-		 */
-		this.setTitle = function (title) {
-			// remove old title
-			$(this.header).find('h4.modal-title').remove();
-			// add new title
-			$(this.header).append('<h4 class="modal-title">' + title + '</h4>');
-		};
-	}
+	// 	/**
+	// 	 * Set modal title
+	// 	 * @param {string} title The title of modal
+	// 	 */
+	// 	this.setTitle = function (title) {
+	// 		// remove old title
+	// 		$(this.header).find('h4.modal-title').remove();
+	// 		// add new title
+	// 		$(this.header).append('<h4 class="modal-title">' + title + '</h4>');
+	// 	};
+	// }
 	
 	
 //these can't be used for instances of offCanvas but we should override them in the code that creates the instance.
@@ -108,4 +108,4 @@ $(document).ready(function () {
 			bsOverlay.removeClass('show');
 		return false;
 	});
-});
+
