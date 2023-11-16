b6b9fdaf5b12c161159974819cb86e6b34dcacc0
Aaron Kennedy <aaron@dmkdesign.ca>
2023-11-16 11:06:44 -0800

    Update OffCanvas.php

--- src/OffCanvas.php
+++ src/OffCanvas.php
@@ -12,10 +12,6 @@ class OffCanvas extends \yii\base\Widget
     const TYPE_DEFAULT = 'default';
     const TYPE_RIGHT = 'right';
     const TYPE_LEFT = 'left';
-
-    public $width=''; //default to css
-    public $includeBackdrop=true;
-    public $location = self::TYPE_RIGHT;
     //sidebar location
     //TODO add location options and width
     public function init()
@@ -23,9 +19,21 @@ class OffCanvas extends \yii\base\Widget
 
         parent::init();
         $view = $this->getView();
-        
+        $styleOverride = "";
         OffCanvasAsset::register($view);
-        
+        if(!empty($this->options['width']))
+        {
+            $styleOverride = " style='width:".$this->options['width']."; margin-".$this->options['location'].":-".$this->options['width']."' ";
+        }
+        $content = 
+            '<div id="bs-canvas-'.$this->options['location'].'" class="bs-canvas bs-canvas-'.$this->options['location'].' position-fixed bg-light h-100" '.$styleOverride.'data-width="'.$this->width.'">
+                <header class="modal-header bs-canvas-header p-3 overflow-auto">
+                    <button type="button" class="bs-canvas-close close" aria-label="Close" aria-expanded="false"><span aria-hidden="true" >&times;</span></button>
+                    <h4 class="modal-title d-inline-block mb-0 ">Canvas Header</h4>
+                </header>
+                <div class="modal-body bs-canvas-content px-3 py-4"> 
+                <!-- Off-canvas content -->';
+        echo $content;
     }
 
     /**
@@ -33,22 +41,29 @@ class OffCanvas extends \yii\base\Widget
      * with the heading and panel containers
      */
     public function run()
-    {   $styleOverride = "";
-        if(!empty($this->width))
-        {
-            $styleOverride = " style='width:".$this->width."; margin-".$this->location.":-".$this->width."' ";
-        }
+    {   
+
        $content = 
-            '<div id="bs-canvas-'.$this->location.'" class="bs-canvas bs-canvas-'.$this->location.' position-fixed bg-light h-100" '.$styleOverride.'data-width="'.$this->width.'">
-                <header class="modal-header bs-canvas-header p-3 overflow-auto">
-                    <button type="button" class="bs-canvas-close close" aria-label="Close" aria-expanded="false"><span aria-hidden="true" >&times;</span></button>
-                    <h4 class="modal-title d-inline-block mb-0 ">Canvas Header</h4>
-                </header>
-                <div class="modal-body bs-canvas-content px-3 py-4">
-                    <!-- Off-canvas content -->
-                </div>   
+            '   </div>   
                 <div class="modal-footer px-3 py-4"><!-- Off-canvas footer --></div>    
             </div>';
         echo $content;
     } 
+
+    /**
+     * Initializes the widget options.
+     * This method sets the default values for various options.
+     */
+    protected function initOptions()
+    {
+        /*
+        public $width=''; //default to css
+    public $includeBackdrop=true;
+    public $location = self::TYPE_RIGHT; */
+        $this->options = array_merge([
+            'width' => '',
+            'includeBackdrop' => true,
+            'location' =>self::TYPE_RIGHT
+        ], $this->options);
+    }
 }
